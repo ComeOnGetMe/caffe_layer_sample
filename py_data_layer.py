3fa2a9f7f716356287d1806d8ece5c739e646e26
ComeOnGetMe <yzp451312410@gmail.com>
2015-11-24 20:56:10 +0800

    Data layer 2nd commit

--- py_data_layer.py
+++ py_data_layer.py
@@ -1,4 +1,3 @@
-import csv
 import caffe
 import numpy as np
 
@@ -7,8 +6,13 @@ class GeneralDataLayer(BasePythonLayer):
     def setup(self, bottom, top):
         assert(len(bottom) == 0)
         assert(len(top) >= 1)
-        self._csvfile, self._delimiter, self._quotechar = self.param_str.split()
-
+        config = []
+        with open(self.param_str, 'r') as config_file:
+            for line in config_file:
+                config.append(line)
+        self._csvfile, self._minibatch_size, self._delimiter = config
+        self._have_read = 0
+        
     def reshape(self, bottom, top):
         pass
 
@@ -18,11 +22,18 @@ class GeneralDataLayer(BasePythonLayer):
             raise NotImplemented
 
     def backward(self):
-        raise Exception('Data Layer does not have backward layer.')
+        raise Exception('Data Layer does not have backward function.')
 
     def prefetch(self):
-        self._data_buffer = []
+        self._data_buffer = self._minibatch_reader()
+
+    def _minibatch_reader(self):
+        bffr = []
         with open(self._csvfile, 'rb') as csvf:
-            datareader = csv.reader(csvf, delimiter = self._delimiter, quotechar = self._quotechar)
-            for line in datareader:
-                self._data_buffer.append(line)
+            csvf.seek(self._have_read)
+            for line in csvf:
+                bffr.append(line.split(self._delimiter))
+                if len(bffr) >= self._minibatch_size:
+                    break
+            self._have_read = csvf.tell()
+        return bffr
